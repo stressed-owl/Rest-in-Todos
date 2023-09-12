5b2c9fd16ec6f2490711082e342ee348d325b715
Nikolay Ponomarenko <nikolay.pon9912@gmail.com>
2023-09-12 22:20:49 +0300

    Custom theme for app was added

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -5,22 +5,25 @@
  */
 
 // Styles
-import '@mdi/font/css/materialdesignicons.css'
-import 'vuetify/styles'
+import "@mdi/font/css/materialdesignicons.css";
+import "vuetify/styles";
 
 // Composables
-import { createVuetify } from 'vuetify'
+import { createVuetify } from "vuetify";
+
+const customLightTheme = {
+  dark: false,
+  colors: {
+    primary: "#F57C00",
+  },
+};
 
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 export default createVuetify({
   theme: {
+    defaultTheme: "customLightTheme",
     themes: {
-      light: {
-        colors: {
-          primary: '#1867C0',
-          secondary: '#5CBBF6',
-        },
-      },
+      customLightTheme,
     },
   },
-})
+});
