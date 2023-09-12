7b7c548e235f68cdda9cdf4fd16d2d70075e2f86
Nikolay Ponomarenko <nikolay.pon9912@gmail.com>
2023-09-12 22:22:30 +0300

    Added methods and models for to-dos

--- src/store/store.js
+++ src/store/store.js
@@ -5,13 +5,60 @@ import { ref } from "vue";
 export const useTodosStore = defineStore("todos", () => {
   const todos = ref([]);
 
-  const addTodo = (todo) => {
-    todos.value.push(todo);
+  const task = ref("");
+  const description = ref("");
+  const editTask = ref("");
+
+  const id = ref(1);
+
+  const isDeleteDialogVisible = ref(false);
+  const isEditDialogVisible = ref(false);
+  const isDeleteConfirmed = ref(false);
+  const isEditTaskConfirmed = ref(false);
+
+  const addTodo = () => {
+    const todo = {
+      id: id.value++,
+      task: task.value,
+      description: description.value,
+      date: new Date().toLocaleTimeString(),
+    };
+
+    if (task.value.trim().length > 0) {
+      todos.value.push(todo);
+    }
+
+    task.value = "";
+    description.value = "";
   };
 
   const deleteTodo = (id) => {
-    todos.value.splice(id, 1);
+    console.log(id);
+    todos.value = todos.value.filter((todo) => todo.id !== id);
   };
 
-  return { todos, deleteTodo, addTodo };
+  const editTodo = (id) => {
+    const todo = todos.value.find((todo) => todo.id === id);
+    if (todo) {
+      const updatedTodo = {
+        ...todo,
+        task: editTask.value,
+      };
+      todos.value = [...todos, updatedTodo];
+    }
+  };
+
+  return {
+    deleteTodo,
+    addTodo,
+    editTodo,
+    todos,
+    task,
+    description,
+    editTask,
+    isDeleteDialogVisible,
+    isEditDialogVisible,
+    isDeleteConfirmed,
+    isEditTaskConfirmed,
+  };
 });
